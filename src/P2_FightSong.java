aadcf9ac731091334ae1d72242b41038b48b5db0
Barbara Nash <bnash1@live.maryville.edu>
2018-04-01 16:50:19 -0500

    string 2

--- src/P2_FightSong.java
+++ src/P2_FightSong.java
@@ -1,8 +1,4 @@
-/*
-  	ISYS 320
-  	Name(s):
-  	Date: 
-*/
+
 
 public class P2_FightSong {
 	public static void main(String[] args) {
@@ -32,3 +28,53 @@ public class P2_FightSong {
 		System.out.println();
 	}
 }
+/*********************************************
+  	ISYS 320
+  	Name(s): Barbara Nash
+  	Date: April 1, 2018
+
+//File: FightSong.java
+public class FightSong {
+The following printGoTeamGo method displays the below two lines of the fight song.
+Go, tean, go!
+You can do it.
+///pringGoTeamGo method implementation
+ * public static viod pringGoTeamGo()
+ * {
+ * system.out.println("Go, team, go!");
+ * system.out.println("You can do it.");
+ * }//end of printGoTeamGo method
+ * The following printYouAreTheBest method displays the below two lines of the fight song.
+ * You're the best,
+ * In the west.
+ //printYouAreTheBest method implementation
+ public static void printYourAreTheBest()
+ {
+ system.out.println("You\'re the best,")
+ system.out.println("In the West.);
+ }//end of printYouAreTheBest method
+ The following main method displays the fight song by calling the printGoTeamGo and pringYouAreTheBest methods.
+ //start main method
+  * public statid void main(String{} args)
+  * {
+  //call the pringGoTeamGo method
+  pringGoTeamGo();
+  //display an empty line
+  system.out.println();
+  //call the pringGoTeamGo method
+   * printGoTeamGo():
+   //call the pringYouAreTheBest method
+    * printYouAreTheBest();
+    //call the pringGoTeamGo method
+    pringGoTeamGo();
+    //display an empty line
+     * system.out.pringln();
+     //call the printGoTeamGo method
+     ringGoTeamGo();
+     }//end of main method
+     }//end of FightSong
+    
+
+   
+
+
