8812f6cdcf24c0756a32bf289062652e560813f0
Barbara Nash <bnash1@live.maryville.edu>
2018-04-01 16:22:30 -0500

    string 1

--- src/P1_TwoCities.java
+++ src/P1_TwoCities.java
@@ -1,7 +1,7 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name: Barbara Nash
+  	Date: April 1, 2018
 */
 
 public class P1_TwoCities {
@@ -12,3 +12,10 @@ public class P1_TwoCities {
 	}
 }
 
+public class P1_TwoCities2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("It was the best of times, \nit was the \"worst\" of times, \nit was the age of wisdom\\foolishness, \netc.");
+	}
+}
